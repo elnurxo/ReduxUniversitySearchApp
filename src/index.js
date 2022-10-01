1d03041419c03b0435a37b1a8a376ffaccb07ddc
elnurxo <elnurykh@code.edu.az>
2022-10-01 19:36:27 +0400

    applying middleware and creating store in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { reducers } from "./redux";
+import thunk from "redux-thunk";
+import "antd/dist/antd.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//creating university Store, applying MiddleWare...
+const universityStore = createStore(reducers, applyMiddleware(thunk));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={universityStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
